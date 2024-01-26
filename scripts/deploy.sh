cb76851dede78877482b54d8836e8b8ad08f4256
Bellanov Apilli <bellanovapilli@gmail.com>
2024-01-26 10:41:58 -0500

    Updated typo in script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ GOOGLE_APPLICATION_CREDENTIALS="/home/bella/.config/gcloud/application_default_c
 
 sudo docker run -e PORT --env PORT=3000 \
     -e EDITOR_UPSTREAM_RENDER_URL --env EDITOR_UPSTREAM_RENDER_URL=3001 \
-    -e GOOGLE_APPLICATION_CREDENTIALS --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/keys/credentials.json.json \
+    -e GOOGLE_APPLICATION_CREDENTIALS --env GOOGLE_APPLICATION_CREDENTIALS=/tmp/keys/credentials.json \
     -v $GOOGLE_APPLICATION_CREDENTIALS:/tmp/keys/credentials.json:ro \
     -p 3000:3000 editor:local
